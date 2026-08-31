e8bfe94a34c3e0cb6deffe22da8dcdb7898d5773
islomxonuzbek-collab <islomxonuzbek@gmail.com>
2026-08-31 21:16:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,17 +6,22 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import BOT_TOKEN
+from database import init_db
 from handlers import user_handlers, admin_handlers
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def main():
+    init_db()
+
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
-    dp.include_router(user_handlers.router)
+    # admin_handlers birinchi bo'lishi kerak, aks holda kod matnlari
+    # (foydalanuvchi handleri) admin FSM holatlarini tutib qolishi mumkin
     dp.include_router(admin_handlers.router)
+    dp.include_router(user_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     logging.info("Bot ishga tushdi...")

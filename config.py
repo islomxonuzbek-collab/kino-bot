860278850e93f6ac66b32d3d9698ca3eec71a877
islomxonuzbek-collab <islomxonuzbek@gmail.com>
2026-08-31 21:16:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@zangorafilm")
 
+# Reklama tugmasi bosilganda foydalanuvchi shu admin bilan yozishmaga o'tadi
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "auwsn")
+
 # Admin telegram ID lari (vergul bilan ajratilgan), masalan: ADMIN_IDS=123456789,987654321
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip().isdigit()]
 
